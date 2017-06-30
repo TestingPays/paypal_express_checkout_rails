174e8f295a99b68b04fd3e616b80eaff50576c80
James Byrne <jamesdesbyrne@gmail.com>
2017-06-30 17:28:45 +0100

    Maknig the new payment page the default

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'new_payment/index'
+
+  root 'new_payment#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
